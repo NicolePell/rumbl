6c9bb278d4cea874cc1255e28ef679e0f560f578
Nicole Pellicena <nicole.pellicena@unrulygroup.com>
2017-06-05 08:50:57 +0100

    Switch to user database instead of in-memory repository.

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,28 +1,3 @@
 defmodule Rumbl.Repo do
-  
-  @moduledoc """
-  In memory repository.
-  """
-
-  def get(module, id) do
-    Enum.find all(module), fn map ->
-      map.id == id end
-  end
-
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end
-  end
-
-  def all(Rumbl.User) do
-    [
-      %Rumbl.User{id: "1", name: "Sharkey Anchorbottom", username: "sharkey", password: "nicole"},
-      %Rumbl.User{id: "2", name: "Sea-Legs Swordskull", username: "sealegs", password: "gel"},
-      %Rumbl.User{id: "3", name: "Gristle Rattlebones", username: "gristle", password: "naveed"},
-      %Rumbl.User{id: "4", name: "Shudders Swindles", username: "shudders", password: "hannes"}
-    ]
-  end
-  def all(_module), do: []
-
+  use Ecto.Repo, otp_app: :rumbl
 end
