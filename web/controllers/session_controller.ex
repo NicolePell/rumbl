3beff082e60c71ad45e54c9a784f05466274ac50
Nicole Pellicena <nicole.pellicena@unrulygroup.com>
2017-06-10 21:01:09 +0100

    Add Logout functionality and logout form.

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -16,6 +16,11 @@ defmodule Rumbl.SessionController do
         |> put_flash(:error, "Invalid username or password combination")
         |> render("new.html")
     end
-    
+  end
+
+  def delete(conn, _) do
+    conn
+    |> Rumbl.Auth.logout()
+    |> redirect(to: page_path(conn, :index))
   end
 end
