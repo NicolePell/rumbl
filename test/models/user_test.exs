a674d3d0ebd35fbfeaaa6d0b5a0421cfc2f78257
Nicole Pellicena <nicole.pellicena@unrulygroup.com>
2017-06-18 11:10:21 +0100

    Test password policies when registering users.

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -6,20 +6,43 @@ defmodule Rumbl.UserTest do
   @valid_attrs %{name: "Sharkey Anchorbottom", username: "sharkey", password: "secret"}
   @invalid_attrs %{}
 
-  test "changeset with valid attributes" do
-    changeset = User.changeset(%User{}, @valid_attrs)
-    assert changeset.valid?
+  describe "changesets" do
+    test "changeset with valid attributes" do
+      changeset = User.changeset(%User{}, @valid_attrs)
+      assert changeset.valid?
+    end
+
+    test "changeset with invalid attributes" do
+      changeset = User.changeset(%User{}, @invalid_attrs)
+      refute changeset.valid?
+    end
+
+    test "changeset does not accept long usernames" do
+      attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 30))
+
+      assert {:username, "should be at most 20 character(s)"} in
+            errors_on(%User{}, attrs)
+    end
   end
 
-  test "changeset with invalid attributes" do
-    changeset = User.changeset(%User{}, @invalid_attrs)
-    refute changeset.valid?
-  end
+  describe "password policies" do
+    test "registration_changeset errors when password is not long enough" do
+      attrs = Map.put(@valid_attrs, :password, "12345")
+      changeset = User.registration_changeset(%User{}, attrs)
 
-  test "changeset does not accept long usernames" do
-    attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 30))
-    assert {:username, "should be at most 20 character(s)"} in
-           errors_on(%User{}, attrs)
+      assert {:password, {"should be at least %{count} character(s)",
+          [count: 6, validation: :length, min: 6]}} in changeset.errors
+    end
+
+    test "registration_changeset successfully hashes password with valid attributes" do
+      attrs = Map.put(@valid_attrs, :password, "123456")
+      changeset = User.registration_changeset(%User{}, attrs)
+      %{password: pass, password_hash: pass_hash} = changeset.changes
+
+      assert changeset.valid?
+      assert pass_hash
+      assert Comeonin.Bcrypt.checkpw(pass, pass_hash)
+    end
   end
 
 end
